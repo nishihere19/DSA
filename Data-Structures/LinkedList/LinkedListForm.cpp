7bd44a336e1ff187f98cda92d7ffb984ac98cda2
Nishi Agrawal <nishihere19@gmail.com>
2021-01-06 14:01:42 +0530

    Function added for reversing a LL

--- Data-Structures/LinkedList/LinkedListForm.cpp
+++ Data-Structures/LinkedList/LinkedListForm.cpp
@@ -31,6 +31,76 @@ void push_at_end(Node **Head, int data){
     }
     
 }
+
+void nth_node_from_end(Node **Head, int n){
+    int length=0; //TO CALCULATE LENGTH OF LINKEDLIST
+    Node *temp = *Head;
+    if(temp==NULL){
+        cout<<"LinkedList was empty";
+        return;
+    }
+    while(temp->next!=NULL){
+        temp=temp->next;
+        length++;
+    }
+    temp=*Head;
+    if(n>length){
+        cout<<"Length of linkedlist is less than input"<<endl;
+        return;
+    }
+    else{
+        for(int i=0;i<length-n+1;i++){
+            temp=temp->next;
+        }
+        cout<<temp->data<<endl;
+
+    }
+    return;
+}
+void nth_node_from_start(Node **Head, int n){
+    int length=0; //TO CALCULATE LENGTH OF LINKEDLIST
+    Node *temp = *Head;
+    if(temp==NULL){
+        cout<<"LinkedList was empty";
+        return;
+    }
+    while(temp->next!=NULL){
+        temp=temp->next;
+        length++;
+    }
+    temp=*Head;
+    if(n>length){
+        cout<<"Length of linkedlist is less than input"<<endl;
+        return;
+    }
+    else{
+        for(int i=0;i<n-1;i++){
+            temp=temp->next;
+        }
+        cout<<temp->data<<endl;
+    }
+    return;
+}
+
+void reverse(Node **Head){
+    Node *prev=new Node();
+    Node *next= new Node();
+    Node *curr= new Node();
+    curr=*Head;
+    next=curr->next;
+    prev= NULL;
+    while(next!=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+        *Head=next;
+        next=next->next;
+        //cout<<prev->data<<curr->data<<next->data<<endl;
+    }
+    curr->next=prev;
+}
+
  int main(){
      struct Node *head= NULL;
      push_at_end(&head,20);
@@ -42,6 +112,25 @@ void push_at_end(Node **Head, int data){
      // THE OUTPUT SHOULD BE 50,40,20,10,30,40
      Node* temp= new Node();
      temp = head;
+     cout<<"The linkedlist is:"<<endl;
+     while(temp!=NULL) {
+         cout<<temp->data<<endl;
+         temp= (temp->next);
+     }
+
+    cout<<"The nth nodes based on input are"<<endl;
+    nth_node_from_start(&head,2);
+    nth_node_from_end(&head,2);
+    nth_node_from_end(&head,1);
+    nth_node_from_start(&head,1);
+    nth_node_from_end(&head,5);
+    nth_node_from_start(&head,5);
+
+    reverse(&head);
+     //Node* temp= new Node();
+     temp = head;
+     //cout<<temp->next<<endl;
+     cout<<"The reversed LinkedList is:"<<endl;
      while(temp!=NULL) {
          cout<<temp->data<<endl;
          temp= (temp->next);
